c344a44b4cc30459972378d4c3360e81e7a9d018
Natalya <m_d.r.lecter@protonmail.com>
2023-03-19 01:20:14 +0400

    feedback form, additional json fields

--- src/view/form-view.js
+++ src/view/form-view.js
@@ -3,34 +3,51 @@ import AbstractView from '../utils/view/abstract-view';
 const createOptionTemplate = (option, type) => (
   `<div class="group-answer radio">
     <label for=${option.id} class="radiolabel">${option.option}</label>
-    <input type="${type}" class="radiobutton" id=${option.id} name="radiobutton" value="${option.price}">
+    <input type="${type}" class="radiobutton data" id=${option.id} name="radiobutton" value="${option.price}">
   </div>`
 );
 
-const createFormTemplate = (question, currentQuestion, amount) => {
+const createDataQuestionTemplate = (price) => (
+  `<p>Примерная стоимость Вашего проекта: ${price}</p>
+  <div class="group-answer radio">
+    <label for="username" class="radiolabel">Введите Ваше имя:</label>
+    <input type="text" class="data" name="radiobutton" id="username">
+  </div>
+  <div class="group-answer radio">
+    <label for="mail" class="radiolabel">Введите Ваш Email:</label>
+    <input type="email" class="data" name="radiobutton" id="mail">
+  </div>
+  `
+);
+
+const createFormTemplate = (question, currentQuestion, amount, price) => {
   const {options, type} = question;
   return (
     `<div class="cta-header-container">
-      <form class="radio-group">
-        ${options.map((option) => createOptionTemplate(option, type)).join('')}
-      </form>
-      <button class="button-next navigation">${currentQuestion === amount - 1 ? 'Рассчитать' : 'Дальше'}</button>
+    ${currentQuestion <= amount? `<form class="radio-group">
+    ${currentQuestion === amount ? createDataQuestionTemplate(price) : options.map((option) => createOptionTemplate(option, type)).join('')}
+  </form>
+  <button class="button-next navigation">${currentQuestion === amount ? 'Отправить' : 'Дальше'}</button>` : '<p>Спасибо за Ваши ответы! Разработчик свяжется с Вами в ближайшее время.</p>'}
   </div>`
   );
 };
 
 export default class FormView extends AbstractView {
-  #questions = null;
+  #question = null;
   #currentQuestion = null;
+  #amount = null;
+  #price = null;
 
-  constructor(questions, currentQuestion) {
+  constructor(question, currentQuestion, amount, price) {
     super();
-    this.#questions = questions;
+    this.#question = question;
     this.#currentQuestion = currentQuestion;
+    this.#amount = amount;
+    this.#price = price;
   }
 
   get template() {
-    return createFormTemplate(this.#questions[this.#currentQuestion], this.#currentQuestion, this.#questions.length);
+    return createFormTemplate(this.#question, this.#currentQuestion, this.#amount, this.#price);
   }
 
   setButtonClickHandler = (callback) => {
@@ -39,7 +56,7 @@ export default class FormView extends AbstractView {
   };
 
   #buttonClickHandler = (evt) => {
-    const inputs = document.querySelectorAll('.radiobutton');
+    const inputs = document.querySelectorAll('.data');
     evt.preventDefault();
     this._callback.buttonClick(inputs, evt);
   };
--- src/view/question-view.js
+++ src/view/question-view.js
@@ -4,7 +4,7 @@ const createQuestionTemplate = (question) => {
   const {paragraph} = question;
   return (
     `<div class="question">
-      <p>${paragraph}</p>
+    ${question.length === 0 ? '<p>Заполните форму ниже, чтобы разработчик мог с Вами связаться:</p>' : `<p>${paragraph}</p>`}
     </div>`
   );
 };
