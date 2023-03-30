40a33371f770509638b40e65eb2426ff3df31888
tronixs <92642123+tronixs@users.noreply.github.com>
2023-03-30 05:15:48 +0300

    секция 1 и компетенции

--- markup/ArtemS/js/index.js
+++
@@ -1,9 +0,0 @@
-const upButton = document.querySelector(".footer-up");
-
-upButton.addEventListener('click', function(){
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
