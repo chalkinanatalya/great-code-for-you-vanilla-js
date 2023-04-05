4d0288ac6babcb85f4cea2eb7b406e0d2dd82c69
Natalya <m_d.r.lecter@protonmail.com>
2023-04-05 20:12:09 +0400

    feedback

--- src/portfolio-api-service.js
+++ src/portfolio-api-service.js
@@ -28,6 +28,19 @@ export default class PortfolioApiService extends ApiService {
 
     return parsedResponse;
   };
+
+  sendFeedback = async (answers) => {
+    const response = await this._load({
+      url: 'feedback',
+      method: Method.POST,
+      body: JSON.stringify(answers),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  };
 }
 
 //json-server src/mock/question.json --routes src/mock/routes.json || json-server src/mock/question.json
--- src/presenter/feedback-presenter.js
+++ src/presenter/feedback-presenter.js
@@ -4,9 +4,11 @@ import FeedbackView from '../view/feedback-view';
 export default class FeedbackPresenter {
   #mainContainer = null;
   #feedbackComponent = new FeedbackView();
+  #feedbackModel = null;
 
-  constructor(mainContainer) {
+  constructor(mainContainer, feedbackModel) {
     this.#mainContainer = mainContainer;
+    this.#feedbackModel = feedbackModel;
   }
 
   init = () => {
@@ -15,5 +17,17 @@ export default class FeedbackPresenter {
 
   #renderFeedback = () => {
     render(this.#feedbackComponent, this.#mainContainer, RenderPosition.BEFOREEND);
+    this.#setButtonHandler();
+  };
+
+  #setButtonHandler = () => {
+    this.#feedbackComponent.setSendButtonClickHandler((inputs) => {
+      const inputsArray = Array.from(inputs);
+      this.#feedbackModel.answers = {
+        'name': inputsArray.find((input) => input.id === 'field-name').value,
+        'phone': inputsArray.find((input) => input.id === 'field-phone').value,
+      };
+      this.#feedbackModel.sendFormAnswers();
+    });
   };
 }
