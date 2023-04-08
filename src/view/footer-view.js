ed9a979106bfd8e593c9ad58a0c470e88de0dad3
tronixs <92642123+tronixs@users.noreply.github.com>
2023-04-08 22:21:29 +0300

    проверка 05

--- src/view/footer-view.js
+++ src/view/footer-view.js
@@ -2,7 +2,7 @@ import AbstractView from '../utils/view/abstract-view';
 
 const createFooterViewTemplate = () => (
   `<footer class="container footer-container">
-    <div class="container footer">
+    <div class="container animate__animated wow footer animate__flipInY">
       <span class="copyright">
         &copy<a href="#">GreatCodeForYou</a> 2023</span>
     </div>
