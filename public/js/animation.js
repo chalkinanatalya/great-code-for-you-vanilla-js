47323f2d0e2dea13d021978de79c35e509fc4734
Natalya <m_d.r.lecter@protonmail.com>
2023-04-10 02:18:08 +0400

    fix

--- public/js/animation.js
+++ public/js/animation.js
@@ -2,10 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const scrollItems = document.querySelectorAll('.qualities');
 
   const scrollAnimation = () => {
-    let windowCenter = (window.innerHeight / 3) + window.scrollY;
-    console.log(windowCenter);
-    scrollItems.forEach(el => {
-      let scrollOffset = el.offsetTop + (el.offsetHeight / 3);
+    const windowCenter = (window.innerHeight / 3) + window.scrollY;
+    scrollItems.forEach((el) => {
+      const scrollOffset = el.offsetTop + (el.offsetHeight / 3);
       if (windowCenter >= scrollOffset) {
         el.classList.add('animation-class');
       } else {
